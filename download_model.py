1982db06ed82d1d7d27170f5f6956fe40ae39029
悟能的师兄 <itliuxing@163.com>
2026-05-29 14:47:44 +0800

    0001 初始自定义

---
+++ download_model.py
@@ -0,0 +1,3 @@
+# modelscope SDK model download
+from modelscope import snapshot_download
+snapshot_download('FunAudioLLM/Fun-CosyVoice3-0.5B-2512', local_dir='pretrained_models/Fun-CosyVoice3-0.5B')
